4ab3027151d3be133e3f461dd642dec67ccef843
andrewkuhl <andrewkuhlz@gmail.com>
2023-02-08 11:17:37 -0700

    wave function

--- ProjectFiles/main.c
+++ ProjectFiles/main.c
@@ -10,18 +10,25 @@ void init_pins()
         gpio_init(pin);
         gpio_set_dir(pin, GPIO_OUT);
     }
+}
+
+void wave()
+{
+    init_pins();
+    uint pin = 2;
     while(true){
 
         for(pin=2;pin<10;pin++){
             gpio_put(pin, 1);
+            vTaskDelay(10);
         }
-        vTaskDelay(100);
         for(pin=2;pin<10;pin++){
             gpio_put(pin, 0);
+            vTaskDelay(10);
         }
-        vTaskDelay(100);
     }
 }
+
 void LED_ON(uint PIN)
 {
     gpio_put(PIN, 1);
@@ -31,7 +38,7 @@ int main()
 {
     stdio_init_all();
 
-    xTaskCreate(init_pins, "init_pins", 256, NULL, 1, NULL);
+    xTaskCreate(wave, "wave", 256, NULL, 1, NULL);
     
     vTaskStartScheduler();
 
