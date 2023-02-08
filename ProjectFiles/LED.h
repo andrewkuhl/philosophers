ea6e4498aa0894a15b93f3c5356891e5ad95a5fe
andrewkuhl <andrewkuhlz@gmail.com>
2023-02-08 12:01:27 -0700

    added LED files

---
+++ ProjectFiles/LED.h
@@ -0,0 +1,10 @@
+#ifndef LED_H
+#define LED_H
+
+#include <stdio.h>
+#include "pico/stdlib.h"
+
+void init_pins();
+void wave();
+
+#endif
--- ProjectFiles/main.c
+++ ProjectFiles/main.c
@@ -1,39 +1,18 @@
 #include <FreeRTOS.h>
 #include <task.h>
 #include <stdio.h>
-#include "pico/stdlib.h"
+#include "LED.h"
 
-void init_pins()
-{
-    uint pin = 2;
-    for(pin=2;pin<10;pin++){
-        gpio_init(pin);
-        gpio_set_dir(pin, GPIO_OUT);
-    }
-}
-
-void wave()
+void init()
 {
     init_pins();
-    uint pin = 2;
-    while(true){
-
-        for(pin=2;pin<10;pin++){
-            gpio_put(pin, 1);
-            vTaskDelay(10);
-        }
-        for(pin=2;pin<10;pin++){
-            gpio_put(pin, 0);
-            vTaskDelay(10);
-        }
-    }
+    wave();
 }
-
 int main()
 {
     stdio_init_all();
 
-    xTaskCreate(wave, "wave", 256, NULL, 1, NULL);
+    xTaskCreate(init, "init", 256, NULL, 1, NULL);
     
     vTaskStartScheduler();
 
