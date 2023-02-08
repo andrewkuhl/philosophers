0f353d3d2278def630fcb16e9bf6600abb76dffc
andrewkuhl <andrewkuhlz@gmail.com>
2023-02-08 13:01:52 -0700

    phil?

--- ProjectFiles/main.c
+++ ProjectFiles/main.c
@@ -2,21 +2,32 @@
 #include <task.h>
 #include <stdio.h>
 #include "pico/stdlib.h"
+#include "semphr.h"
+
+SemaphoreHandle_t leds;
 
 void phil(uint pin)
 {
     while(true)
     {
-        gpio_put(pin, 1);
-        vTaskDelay(50);
-        gpio_put(pin,0);
-        vTaskDelay(50);
+        if(xSemaphoreTake(leds, (TickType_t) 10) == pdTRUE){
+            gpio_put(pin, 1);
+            vTaskDelay(100);
+            gpio_put(pin, 0);
+            xSemaphoreGive(leds);
+            vTaskDelay(10);
+        }
+        else{
+            vTaskDelay(1);
+        }
     }
 }
 
 int main()
 {
     stdio_init_all();
+
+    leds = xSemaphoreCreateCounting(3,3);
 
     for(uint pin=2;pin<10;pin++){
         gpio_init(pin);
