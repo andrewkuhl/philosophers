b40cc17dc42fb943eb9cc10221a2355c5c8b9514
andrewkuhl <andrewkuhlz@gmail.com>
2023-02-08 11:11:43 -0700

    test all lights turn on

--- ProjectFiles/main.c
+++ ProjectFiles/main.c
@@ -6,8 +6,21 @@
 void init_pins()
 {
     uint pin = 2;
-    gpio_init(pin);
-    gpio_set_dir(pin, GPIO_OUT);
+    for(pin=2;pin<10;pin++){
+        gpio_init(pin);
+        gpio_set_dir(pin, GPIO_OUT);
+    }
+    while(true){
+
+        for(pin=2;pin<10;pin++){
+            gpio_put(pin, 1);
+        }
+        vTaskDelay(100);
+        for(pin=2;pin<10;pin++){
+            gpio_put(pin, 0);
+        }
+        vTaskDelay(100);
+    }
 }
 void LED_ON(uint PIN)
 {
@@ -19,7 +32,6 @@ int main()
     stdio_init_all();
 
     xTaskCreate(init_pins, "init_pins", 256, NULL, 1, NULL);
-    xTaskCreate(LED_ON, "led on", 256, 2, 1, NULL);
     
     vTaskStartScheduler();
 
