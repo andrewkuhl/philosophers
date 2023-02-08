7763a9924d950ed373f5e73342a4dba55d8c2ebc
andrewkuhl <andrewkuhlz@gmail.com>
2023-02-08 13:57:12 -0700

    favorite pattern

--- ProjectFiles/main.c
+++ ProjectFiles/main.c
@@ -12,7 +12,7 @@ void phil(uint pin)
     {
         if(xSemaphoreTake(leds, (TickType_t) 10000) == pdTRUE){
             gpio_put(pin, 1);
-            vTaskDelay(5);
+            vTaskDelay(50);
             gpio_put(pin, 0);
             xSemaphoreGive(leds);
             vTaskDelay(1);
@@ -27,7 +27,7 @@ int main()
 {
     stdio_init_all();
 
-    leds = xSemaphoreCreateCounting(1,1); // AMOUNT OF CHOPSTICKS
+    leds = xSemaphoreCreateCounting(3,3); // AMOUNT OF CHOPSTICKS
 
     for(uint pin=2;pin<10;pin++){
         gpio_init(pin);
